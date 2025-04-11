d04bbd9293ee1a63920c2c7f3ccb606746311cdf
Kuncha Vamshi Reddy <vamshireddy594@gmail.com>
2025-04-11 22:44:08 +0530

    Practise

--- test.py
+++ test.py
@@ -9,3 +9,12 @@
 # a,b=b,a
 # print(a,b)
 
+
+
+# def print_adjaceny_list(V, edges):
+#     adj_list = {i: [] for i in range(V)}
+#     print(adj_list)
+# V=3
+# edges=[[0,1],[1,2],[2,3]]
+# print_adjaceny_list(V,edges)
+
